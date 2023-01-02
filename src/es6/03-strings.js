39ac7868b296469dc9c692b77bf6827431c70648
Arnol Plazas <plazasarnol@gmail.com>
2023-01-02 16:38:17 -0500

    ES6: string-> template literals.

---
+++ src/es6/03-strings.js
@@ -0,0 +1,20 @@
+
+let hello = 'Hello';
+let world = 'World'
+
+let epicPhrase = hello + ' ' + world + '!'
+console.log(epicPhrase);
+
+
+// template literals
+let epicPhrase2 = `${hello} ${world}!`
+console.log(epicPhrase2);
+
+// multi-line strings
+let lorem = 'this is a string \n' + 'this is a new line';
+let lorem2 = `this a epic phrase
+here continue phrase.
+`;
+
+console.log(lorem);
+console.log(lorem2);
