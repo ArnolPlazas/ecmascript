8f7cd64266bc40147ee783c478066518d91aff91
Arnol Plazas <plazasarnol@gmail.com>
2023-01-05 07:59:24 -0500

    ES6: spread operator.

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -8,3 +8,18 @@ let user = {username: 'Arnol', age: 34}
 let { username, age } = user;
 console.log(username, age);
 
+// spread operator
+let person = {name: 'Arnol', age: 28}
+let country = 'CO'
+let data = {id: 1, ...person, country}
+console.log(data);
+
+
+// rest
+function sum(num, ...values) {
+    console.log(values);
+    console.log(num + values[0]);
+}
+
+sum(1, 1, 2, 3, 4);
+
