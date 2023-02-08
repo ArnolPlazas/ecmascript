d189a01564cecd4626eed92a5d9c2d73b6fa6f32
Arnol Plazas <plazasarnol@gmail.com>
2023-02-08 07:03:16 -0500

    ES8: string padding y trailing commas.

---
+++ src/es8/03-string-padding.js
@@ -0,0 +1,6 @@
+const string ='Hello'
+// llenar string al inicio con el caracter _ hasta obtener un string nuevo con 10 caracteres
+console.log(string.padStart(10, '_')); // ____hello
+
+// llenar string al final con el caracter _ hasta obtener un string nuevo con 10 caracteres
+console.log(string.padEnd(10, '_')); // hello____
---
+++ src/es8/04-trailing-commas.js
@@ -0,0 +1,4 @@
+// agregar posiciones vacias en un array
+const age = [24, 25, 32, 33, , , 45]
+console.log(age); // [ 24, 25, 32, 33, <2 empty items>, 45 ]
+console.log(age.length); // 7
